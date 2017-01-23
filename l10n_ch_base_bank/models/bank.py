7d5bcfbc8e94797af3c81f30d55944d11be3500d
stephanamann <stephanamann@users.noreply.github.com>
2017-01-23 15:20:20 +0100

    Update bank.py

--- l10n_ch_base_bank/models/bank.py
+++ l10n_ch_base_bank/models/bank.py
@@ -5,7 +5,7 @@ import re
 from openerp import models, fields, api, _
 from openerp.tools import mod10r
 from openerp import exceptions
-
+from openerp.addons.base_iban import base_iban
 
 class BankCommon(object):
 
@@ -52,6 +52,7 @@ class BankCommon(object):
         """
         if not iban[:2] == 'CH':
             return False
+        iban = base_iban.normalize_iban(iban)
         part1 = iban[-9:-7]
         part2 = iban[-7:-1].lstrip('0')
         part3 = iban[-1:].lstrip('0')
